324b6ce064043e67777f33cdb3ede58ba1a51a2d
iyyappanmanivelan <iyyappanmani03@gmail.com>
2025-09-17 00:16:42 +0530

    new pull

---
+++ src/Common/SessionCard/index.jsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+const SessionCard = ({ type, title, speaker, overview, date, amount }) => {
+  return <div></div>;
+};
+
+export default SessionCard;
---
+++ src/Constant/Session/index.js
@@ -0,0 +1,87 @@
+export const Session = [
+  {
+    id: 1,
+    type: "Workshop",
+    title: "Advanced Cataract Surgery Techniques",
+    speaker: "Dr. Priya Sharma",
+    date: "2024-04-23",
+    time: "10.00 am - 1.00 pm",
+    overview:
+      "Interactive discussion on artificial intelligence applications in ophthalmology, including diagnostic imaging, predictive analytics, and automated",
+    amount: 2000,
+  },
+  {
+    id: 2,
+    type: "Workshop",
+    title: "Advanced Cataract Surgery Techniques",
+    speaker: "Dr. Priya Sharma",
+    date: "2024-04-23",
+    time: "10.00 am - 1.00 pm",
+    overview:
+      "Interactive discussion on artificial intelligence applications in ophthalmology, including diagnostic imaging, predictive analytics, and automated",
+
+    amount: 2000,
+  },
+  {
+    id: 3,
+    type: "Round Table",
+    title: "Advanced Cataract Surgery Techniques",
+    speaker: "Dr. Priya Sharma",
+    date: "2024-04-23",
+    time: "10.00 am - 1.00 pm",
+    overview:
+      "Interactive discussion on artificial intelligence applications in ophthalmology, including diagnostic imaging, predictive analytics, and automated",
+
+    amount: 2000,
+  },
+  {
+    id: 4,
+    type: "Round Table",
+    title: "Advanced Cataract Surgery Techniques",
+    speaker: "Dr. Priya Sharma",
+    date: "2024-04-23",
+    time: "10.00 am - 1.00 pm",
+    overview:
+      "Interactive discussion on artificial intelligence applications in ophthalmology, including diagnostic imaging, predictive analytics, and automated",
+
+    amount: 2000,
+  },
+  {
+    id: 5,
+    type: "Workshop",
+    title: "Advanced Cataract Surgery Techniques",
+    speaker: "Dr. Priya Sharma",
+    date: "2024-04-23",
+    time: "10.00 am - 1.00 pm",
+    overview:
+      "Interactive discussion on artificial intelligence applications in ophthalmology, including diagnostic imaging, predictive analytics, and automated",
+
+    amount: 2000,
+  },
+
+  {
+    id: 6,
+    type: "Workshop",
+    title: "Advanced Cataract Surgery Techniques",
+    speaker: "Dr. Priya Sharma",
+    date: "2024-04-23",
+    time: "10.00 am - 1.00 pm",
+    overview:
+      "Interactive discussion on artificial intelligence applications in ophthalmology, including diagnostic imaging, predictive analytics, and automated",
+
+    amount: 2000,
+  },
+
+  {
+    id: 7,
+    type: "Workshop",
+    title: "Advanced Cataract Surgery Techniques",
+    speaker: "Dr. Priya Sharma",
+    date: "2024-04-23",
+    time: "10.00 am - 1.00 pm",
+    overview:
+      "Interactive discussion on artificial intelligence applications in ophthalmology, including diagnostic imaging, predictive analytics, and automated",
+
+    amount: 2000,
+  },
+];
